c25afb7d205adad5fc4dcd8c896909334805c06a
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-10 11:23:54 -0500

    feat: add enigma create_offsets and create_final_keys methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,20 +1,34 @@
 class Enigma
+  attr_reader :character_set
 
   def initialize
+    @character_set = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key, date)
     create_keys(key)
+    create_offsets(date)
+    create_final_keys
+    encrypted_array = message.split("").map do |char|
+      binding.pry
+    end
+
 
   end
 
   def create_keys(key)
     key_arr = key.split("")
-    keys = Hash.new
-    keys[:a] = key_arr[0] + key_arr[1]
-    keys[:b] = key_arr[1] + key_arr[2]
-    keys[:c] = key_arr[2] + key_arr[3]
-    keys[:d] = key_arr[3] + key_arr[4]
-    keys
+    @keys = key_arr.each_cons(2).map { |key| (key[0] + key[1]).to_i }
+  end
+
+  def create_offsets(date)
+    date_squared = (date.to_i ** 2).to_s
+    @offsets = (date_squared[-4..-1].split("")).map { |digit| digit.to_i }
+  end
+
+  def create_final_keys
+    key_and_offsets = []
+    key_and_offsets << @keys << @offsets
+    final_keys = key_and_offsets.transpose.map { |pair| pair.reduce(:+) }
   end
 end
--- spec/engigma_spec.rb
+++ spec/engigma_spec.rb
@@ -12,13 +12,32 @@ RSpec.describe Enigma do
   end
 
   it 'can create keys based on key input' do
-    expect(@enigma.create_keys("02715").keys.count).to eq 4
-    expect(@enigma.create_keys("02715")).to eq({
-        a: "02",
-        b: "27",
-        c: "71",
-        d: "15"
-      })
+    expect(@enigma.create_keys("02715").count).to eq 4
+    expect(@enigma.create_keys("02715")).to eq([02, 27, 71, 15])
+  end
+
+  it 'can create offsets based on date' do
+    expect(@enigma.create_offsets("040895")).to eq([1, 0, 2, 5])
+  end
+
+  it 'can create a final set of keys based on the keys and offsets' do
+    @enigma.create_keys("02715")
+    @enigma.create_offsets("040895")
+    expect(@enigma.create_final_keys).to eq([3, 27, 73, 20])
+  end
+
+  it 'has a character set' do
+    expect(@enigma.character_set.count).to eq 27
+    expect(@enigma.character_set[0]).to eq "a"
+    expect(@enigma.character_set[-1]).to eq " "
+  end
+
+  it 'can encrypt a single letter' do
+    expect(@enigma.encrypt("h", "02715", "040895")).to eq({
+      encryption: "k",
+      key: "02715",
+      date: "040895"
+    })
   end
 
   xit 'can encrypt a message' do
