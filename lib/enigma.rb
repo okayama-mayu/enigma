7831fa536debae872fa70c68e4db1cc64e9afa93
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-10 10:26:43 -0500

    feat: add Enigma create_keys method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,19 @@ class Enigma
 
   def initialize
   end
+
+  def encrypt(message, key, date)
+    create_keys(key)
+
+  end
+
+  def create_keys(key)
+    key_arr = key.split("")
+    keys = Hash.new
+    keys[:a] = key_arr[0] + key_arr[1]
+    keys[:b] = key_arr[1] + key_arr[2]
+    keys[:c] = key_arr[2] + key_arr[3]
+    keys[:d] = key_arr[3] + key_arr[4]
+    keys
+  end
 end
--- spec/engigma_spec.rb
+++ spec/engigma_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe Enigma do
   it 'is an Enigma' do
     expect(@enigma).to be_instance_of Enigma
   end
+
+  it 'can create keys based on key input' do
+    expect(@enigma.create_keys("02715").keys.count).to eq 4
+    expect(@enigma.create_keys("02715")).to eq({
+        a: "02",
+        b: "27",
+        c: "71",
+        d: "15"
+      })
+  end
+
+  xit 'can encrypt a message' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
+
+  end
+
+  xit 'generates a random key if no key is passed in' do
+    expect(@key.size).to eq 5
+    expect(@key.class).to eq String
+    expect(@key.to_i.class).to eq Integer
+  end
 end
