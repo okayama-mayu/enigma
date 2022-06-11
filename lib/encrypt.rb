c6117560472bdc48feb203216c88b2e1afc688bb
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-11 18:13:52 -0500

    runner: encrypt.rb file methods called

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,11 @@ require 'pry'
 require './lib/enigma'
 require 'date'
 # ARGV == ["message.txt", "encrypted.txt"]
-secret_message = File.open(ARGV[0], "r")
+original_message = File.open(ARGV[0], "r")
 encrypted_message = File.open(ARGV[1], "w")
-secret_text = secret_message.read
-enigma = Enigma.new
-encrypted_text = enigma.encrypt(secret_text)
-encrypted_message.write(encrypted_text[:encryption], encrypted_text[:key])
+original_text = original_message.read
+encrypted_text = Enigma.new.encrypt(original_text)
+encrypted_message.write(encrypted_text[:encryption])
 encrypted_message.close
-binding.pry
+
+puts "Created #{ARGV[1]}"
