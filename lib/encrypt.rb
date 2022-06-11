035c2c9b591eb2f3fd27746ef49b0d194b6244de
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-11 13:22:39 -0500

    feat: use ARGV

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,6 +1,12 @@
 require 'pry'
+require './lib/enigma'
+require 'date'
 # ARGV == ["message.txt", "encrypted.txt"]
-binding.pry 
 secret_message = File.open(ARGV[0], "r")
 encrypted_message = File.open(ARGV[1], "w")
 secret_text = secret_message.read
+enigma = Enigma.new
+encrypted_text = enigma.encrypt(secret_text)
+encrypted_message.write(encrypted_text[:encryption], encrypted_text[:key])
+encrypted_message.close
+binding.pry
